fd887550fd23575070c629fc17d27265fa83b3ac
Jovana <jovanamatic3@gmail.com>
2022-12-27 20:02:10 +0100

    Made some change in exercise Soccer Player

--- EVENTS/soccerPlayer.js
+++ EVENTS/soccerPlayer.js
@@ -1,3 +1,23 @@
 // var body = document.querySelector('body');
 // var button = document.getElementById('button');
 // var player = document.getElementById('img');
+
+// searchBtn.addEventListener('click', searchYT);
+
+var target = document.getElementById('target');
+var targetBox = document.getElementById('target-box');
+
+targetBox.addEventListener('click', mouseClicked);
+
+function mouseClicked (event){
+  var xposition = (event.clientX - target.offsetLeft - target.offsetWidth/2);
+  var yposition = (event.clientY - target.offsetTop - target.offsetHeight/2);
+  target.style.transform = "translate("+ xposition + "px," + yposition + "px)";
+  console.log(event);
+}
+
+
+
+button.addEventListener('click', function onClick(event){
+    document.getElementById('target') = back-to-top 
+})
